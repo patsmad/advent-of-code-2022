54920598f9ca6aebcaea69aa0f4b5f59356803ae
Patrick Smadbeck <patsmad@gmail.com>
2022-12-12 20:39:58 -0500

    day 12

--- day_12.py
+++ day_12.py
@@ -11,15 +11,17 @@ acctuvwj
 abdefghi
 """.strip()
 
-input = test_input
+input = raw_input.split('\n')
 
 # part 1
 
 class Node:
     def __init__(self, letter):
-        self.elevation = self.get_elevation(letter)
-        self.shorted_path_length = None
-        self.shorted_path_node = None
+        self.letter = letter
+        self.elevation = self.get_elevation(self.letter)
+        self.is_start = self.letter == 'S'
+        self.is_end = self.letter == 'E'
+        self.steps = None
         self.edges = []
 
     def get_elevation(self, letter):
@@ -30,22 +32,46 @@ class Node:
         else:
             return ord('z')
 
-nodes = {}
-for i in range(len(input)):
-    for j in range(len(input[i])):
-        nodes[(i,j)] = Node(input[i][j])
-        if i > 0:
-            if nodes[(i,j)].elevation >= nodes[(i-1,j)].elevation:
-                nodes[(i, j)].edges.append(nodes[(i-1,j)])
-            if nodes[(i,j)].elevation <= nodes[(i-1,j)].elevation:
-                nodes[(i-1,j)].edges.append(nodes[(i, j)])
-        if j > 0:
-            if nodes[(i, j)].elevation >= nodes[(i, j-1)].elevation:
-                nodes[(i, j)].edges.append(nodes[(i, j-1)])
-            if nodes[(i, j)].elevation <= nodes[(i, j-1)].elevation:
-                nodes[(i, j-1)].edges.append(nodes[(i, j)])
+def get_nodes(input):
+    input_nodes = {}
+    for i in range(len(input)):
+        for j in range(len(input[i])):
+            input_nodes[(i, j)] = Node(input[i][j])
+            if i > 0:
+                if input_nodes[(i, j)].elevation >= input_nodes[(i-1, j)].elevation - 1:
+                    input_nodes[(i, j)].edges.append(input_nodes[(i-1, j)])
+                if input_nodes[(i, j)].elevation <= input_nodes[(i-1, j)].elevation + 1:
+                    input_nodes[(i-1, j)].edges.append(input_nodes[(i, j)])
+            if j > 0:
+                if input_nodes[(i, j)].elevation >= input_nodes[(i, j-1)].elevation - 1:
+                    input_nodes[(i, j)].edges.append(input_nodes[(i, j-1)])
+                if input_nodes[(i, j)].elevation <= input_nodes[(i, j-1)].elevation + 1:
+                    input_nodes[(i, j-1)].edges.append(input_nodes[(i, j)])
+    return input_nodes
 
-q = []
-print(nodes)
+def get_steps(start_node):
+    q = []
+    q.append(start_node)
+    while len(q) > 0:
+        node = q.pop(0)
+        if node.is_end:
+            return node.steps
+        for child_node in node.edges:
+            if child_node.steps is None:
+                child_node.steps = node.steps + 1
+                q.append(child_node)
+
+nodes = get_nodes(input)
+start_node = [n for n in nodes.values() if n.is_start][0]
+start_node.steps = 0
+steps = get_steps(start_node)
+print(steps)
 
 # part 2
+nodes = get_nodes(input)
+possible_start_nodes = [n for n in nodes.values() if n.elevation == 97]
+pseudo_node = Node('a')
+pseudo_node.steps = -1
+pseudo_node.edges = possible_start_nodes
+steps = get_steps(pseudo_node)
+print(steps)
